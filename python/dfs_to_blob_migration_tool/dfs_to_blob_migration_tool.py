434c92f17b9fafadf2a2518f2cc8f4fc6de354f6
Ruslan Nour <rnour@igloosoftware.com>
2020-02-06 17:10:09 -0500

    Fixing Exception file timestamp

--- python/dfs_to_blob_migration_tool/dfs_to_blob_migration_tool.py
+++ python/dfs_to_blob_migration_tool/dfs_to_blob_migration_tool.py
@@ -123,7 +123,7 @@ def main():
                 blob_added_count += 1
             else:
                 # open exceptions file and put there updated line
-                exceptions_file = "Exceptions_" + country_code + tenant_name + "_" + time.strftime("%Y_%m_%d-%H%M%S") + ".txt"
+                exceptions_file = "Exceptions_" + country_code + tenant_name + "_" + time.strftime("%Y_%m_%d") + ".txt"
                 exceptions = open(exceptions_file, "a+")
                 exceptions.write(source_filename + "\n")
                 exceptions.close()
